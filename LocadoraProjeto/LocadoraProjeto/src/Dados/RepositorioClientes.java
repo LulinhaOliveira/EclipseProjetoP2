30787cf30951a128a92bd40a641777ba9436ce32
Aluno <Aluno@LAB-DEINFO>
2018-11-26 20:08:19 -0300

    UltimaATT

--- LocadoraProjeto/LocadoraProjeto/src/Dados/RepositorioClientes.java
+++ LocadoraProjeto/LocadoraProjeto/src/Dados/RepositorioClientes.java
@@ -5,4 +5,20 @@ import Negocio.Clientes;
 
 public class RepositorioClientes {
 	private ArrayList<Clientes> listaClientes = new ArrayList<>();
+	
+	public void inserirCliente(Clientes cliente) {
+		listaClientes.add(cliente);
+	}
+	
+	public void removerCliente(Cliente cliente) {
+		listaClientes.remove(cliente);
+	}
+	
+	public void buscarCliente(Cliente cliente) {
+		return listaClientes.get(listaVeiculos.indexOf(cliente));
+	}
+	
+	public void atualizarCliente(String nome, Cliente cliente) {
+		listaClientes.add(listaVeiculos.indexOf(cliente.getNome(), cliente));
+	}
 }
--- LocadoraProjeto/LocadoraProjeto/src/Negocio/ControleClientes.java
+++ LocadoraProjeto/LocadoraProjeto/src/Negocio/ControleClientes.java
@@ -1,5 +1,15 @@
 package Negocio;
 
 public class ControleClientes {
-
+	RepositorioClientes clientes;
+	Clientes clienteAux;
+	
+	public void inserirCliente(Clientes cliente) {
+		if(cliente  == null) {
+			
+		} else {
+			this.clientes.add(cliente);
+		} 
+		
+	}
 }
